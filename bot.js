a2a28a31d236271afe2b2c66738cdfa719dc42b2
lordpie <lordpie314@gmail.com>
2022-09-09 14:17:56 -0700

    re-enable bot commands

--- bot.js
+++ bot.js
@@ -283,7 +283,7 @@ client.on('message', async message => {
 
   if (commoncommands == true) {
     switch (commandName) {
-        /*
+        
       case 'unspecguide':
         message.reply('Here is the beginner unspec deck building guide: https://www.reddit.com/r/wargamebootcamp/comments/5m0wmz/meta_a_guide_to_unspec_deckbuilding/');
         break;
@@ -297,7 +297,7 @@ client.on('message', async message => {
       case 'razzguide':
         message.reply("Here is Razzmann's video guides: https://www.youtube.com/playlist?list=PL3d-ZYWK9TPkb8zuvxNRArw1gyi1fgb0R");
         break;
-        */
+        *
       case 'keyvalues':
         message.reply('Here is a link to the Key Values to remember: https://www.reddit.com/r/wargamebootcamp/comments/7oj7nx/list_of_key_values_to_keep_track_of_for_beginners/');
         break;
@@ -307,7 +307,7 @@ client.on('message', async message => {
       case 'replayfolder':
         message.reply('here is the directory for the replay folder: \n\n *Windows: C:\\Users%username%\\Saved Games\\EugenSystems\\WarGame3* \n\nLinux: ~/.config/EugenSystems/Wargame3/saves \n\n Mac: [Hard drive] > Users > [your account] > Library > Application Support > EugenSystems > Wargame3 > SavedGames');
         break;
-        /*
+        
       case 'rof':
         message.reply('Here is a link to the rate of fire spreadsheet: https://docs.google.com/spreadsheets/d/1dx28wRZ_3ofnP7kWKcoziGpPw2tOAJcixnuiKjJPL-A/edit#gid=1401351233');
         break;
@@ -320,7 +320,7 @@ client.on('message', async message => {
       case 'rankedmaps':
         message.reply('1v1: \n\n **Mud fight** \n\n **Plunjing valley** \n\n **Paddy field** \n\n **Punchbowl** \n\n **Hell in a very small place** \n\n **Highway to seoul** \n\n **Nuclear winter**');
         break;
-        */
+        
     }
   }
 });
